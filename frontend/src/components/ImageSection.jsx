daafc47509b2d0a2ed5190880f91ee99ec2c4cfa
MaxEnocMN <max_enoc927@yahoo.com.mx>
2025-09-17 11:37:39 -0600

    Deploying to GitHub Pages

--- frontend/src/components/ImageSection.jsx
+++
@@ -1,9 +0,0 @@
-function ImageSection({ src, alt }) {
-    return (
-        <div className="image-section">
-            <img src={src} alt={alt} className="main-image" />
-        </div>
-    );
-}
-
-export default ImageSection;
