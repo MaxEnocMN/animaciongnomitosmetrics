222ed158c75047b0ddf7ad1916da5fad2bac39e4
MaxEnocMN <max_enoc927@yahoo.com.mx>
2025-09-25 02:47:46 -0600

    Deploy build sin console.error country log

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './frontend',
+  root: '.', 
   build: {
     outDir: 'dist',
     emptyOutDir: true
   },
-  server: {
-    port: 5173
-  }
-});
+  plugins: [react()]
+})
